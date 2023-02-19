c041d616e8f8c09062421e4fbc4c0afe87e577a0
max quinones <maximilian.quinones@gmail.com>
2023-02-19 06:31:20 -0700

    updating

---
+++ src/breed.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Breed = () =>{
+
+  return (
+    <div>hello</div>
+  )
+}
+
+
+export default Breed;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import JSON from './breed.json'
+import Breed from './breed';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component {
+  render() {
+    return (
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <header>
+        <div>Logo</div>
+        <div className='input-now'>input</div>
+        <Breed/>
+        <input/>
+      </header>
+    );
+  }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
